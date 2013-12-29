fc0872ae81e2cae699e8600f07ce271b84e0933f
manosai <mano.eerabathini@gmail.com>
2013-12-29 01:27:50 -0800

    added sys argument for qb name and url

--- data_collector.py
+++ data_collector.py
@@ -1,6 +1,15 @@
+import sys 
 import urllib3
 
+def main(qb_name): 
+	qb_name = qb_name.replace("_", " ") 
+	base_url = 'http://www.pro-football-reference.com/players/'
+	last_char = qb_name.split(" ")[1][0]
+	player_url = qb_name.split(" ")[1][1:4] + qb_name.split(" ")[0][0:2] + "00/"
+	url = base_url + last_char + player_url + "touchdowns/passing/2013"
+	print url 
+	http = urllib3.PoolManager()
+	r = http.request('GET', url)
 
-http = urllib3.PoolManager()
-r = http.request('GET', 'http://www.pro-football-reference.com/players/B/BreeDr00/touchdowns/passing/2013/')
-print r.data
+if __name__ == "__main__":
+	main(sys.argv[1])
